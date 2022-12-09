28ba6082c52792cf8f778d2c5a6ce4b378d9de0f
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2022-12-09 15:57:52 -0500

    added Loader

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Loader from '../components/Loader'
+
 export default function Home() {
   return (
-    <div>Hello Kristin!</div>
+    <Loader show />
   )
 }
