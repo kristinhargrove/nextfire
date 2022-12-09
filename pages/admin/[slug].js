d136d1a37434f14a23e301bb94519abc37d2f36b
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2022-12-09 15:58:02 -0500

    set up routes

---
+++ pages/admin/[slug].js
@@ -0,0 +1,7 @@
+export default function AdminPostEdit({}) {
+  return (
+    <main>
+      <h1>Edit Posts</h1>
+    </main>
+  );
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,7 @@
+export default function AdminPostsPage({ }) {
+  return (
+    <main>
+      
+    </main>
+  )
+}
